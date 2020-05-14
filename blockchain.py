4f09fcff4cf2126a4337d2f9c46dfb2d7f8fb385
fabiano.santos <fabiano.santos@conductor.com.br>
2020-05-14 11:17:16 -0300

    update deploy blockchain

--- blockchain.py
+++ blockchain.py
@@ -229,4 +229,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, port=33507)
+    # app.run(host='0.0.0.0', port=port)
